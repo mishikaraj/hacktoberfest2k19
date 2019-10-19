fb40d59c4965854a18c8cbfb09452b5f30b0fde7
mishikaraj <49092742+mishikaraj@users.noreply.github.com>
2019-10-19 22:31:53 +0530

    Update doublylinked.c

--- Doubly Linked List/doublylinked.c
+++ Doubly Linked List/doublylinked.c
@@ -9,7 +9,17 @@ int searchint(int x);
 
 int compare_fn(int a,int b)
 {
-    //Write the compare function for the variables 'a' and 'b' and return the value
+    if(a>b){
+	    prinf("a is greater");
+    }
+else if(a<b)
+{
+	printf("b is graeter");
+}
+	else 
+		printf("equal");
+
+	    //Write the compare function for the variables 'a' and 'b' and return the value
 }
 
 int compare_no=1;
@@ -17,8 +27,8 @@ int compare_no=1;
 struct node
 {
 	int data;
-	node *prev;
-	node *next;
+	struct node *prev;
+	struct node *next;
 };
 
 //A missing line here which initialises the top condition.
@@ -71,7 +81,7 @@ int insertdata(int x)
 {
     if(top==NULL)
     {
-        //Write a code for this particular condition where TOP == NULL
+        print("overflow");//Write a code for this particular condition where TOP == NULL
     }
     else if(compare_fn(top->data ,x)==compare_no)
     {
