f55d29a450603d348242ab9daf8210eed6315f8e
mishikaraj <49092742+mishikaraj@users.noreply.github.com>
2019-10-20 03:33:28 +0530

    Update Armstrong.c

--- Armstrong.c
+++ Armstrong.c
@@ -1,19 +1,20 @@
-#include<stdio.h>  
-int main()    
-{    
-	int n,r,sum=0,temp;    
-	printf("enter the number=");    
-	scanf("%d",&n);    
-	temp=n;    
-	while(n>0)    
-	{    
-		r=n%10;    
-		sum=sum+(r*r*r);    
-		n=n/10;    
-	}    
-	if(temp==sum)    
-		printf("armstrong  number ");    
-	else    
-		printf("not armstrong number");    
-	return 0;  
-}   
+
+#include <stdio.h>
+int main()
+{
+    int number, originalNumber, remainder, result = 0;
+    printf("Enter a three digit integer: ");
+    scanf("%d", &number);
+    originalNumber = number;
+    while (originalNumber != 0)
+    {
+        remainder = originalNumber%10;
+        result += remainder*remainder*remainder;
+        originalNumber /= 10;
+    }
+    if(result == number)
+        printf("%d is an Armstrong number.",number);
+    else
+        printf("%d is not an Armstrong number.",number);
+    return 0;
+}
